bdd04701858ea41b12fdae5c9aade3b6ce342d16
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-12-07 02:15:09 -0300

    set up project

---
+++ Cinetopia/Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Yuri Cunha on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
