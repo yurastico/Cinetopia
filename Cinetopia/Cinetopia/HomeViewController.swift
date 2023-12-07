7231f532a761fa1786c2d5924a53bc38581c0f22
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-12-07 03:23:54 -0300

    using UIStackview and navigation

--- Cinetopia/Cinetopia/HomeViewController.swift
+++ Cinetopia/Cinetopia/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController {
     private lazy var logoImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage.logo)
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         
         return imageView
     }()
@@ -19,7 +20,7 @@ class HomeViewController: UIViewController {
     private lazy var coupleImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage.couple)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -43,7 +44,24 @@ class HomeViewController: UIViewController {
         button.setTitleColor(.background, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 20)
         button.layer.cornerRadius = 32
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         return button
+    }()
+    
+    
+    @objc private func buttonPressed() {
+        navigationController?.pushViewController(MoviesViewController(), animated: true)
+    }
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [logoImageView,coupleImageView,welcomeLabel,welcomeButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 32
+        stackView.distribution = .fill
+        return stackView
     }()
     
     override func viewDidLoad() {
@@ -53,37 +71,49 @@ class HomeViewController: UIViewController {
         addSubviews()
         setupConstraints()
         
+        
        
     }
     
     
 
     private func addSubviews() {
-        view.addSubview(logoImageView)
-        view.addSubview(coupleImageView)
-        view.addSubview(welcomeLabel)
-        view.addSubview(welcomeButton)
+//        view.addSubview(logoImageView)
+//        view.addSubview(coupleImageView)
+//        view.addSubview(welcomeLabel)
+//        view.addSubview(welcomeButton)
+        
+        view.addSubview(stackView)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 64),
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 32),
+//            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            
+//            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,constant: 32),
+//            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            
+//            welcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor,constant: 32),
+//            
+//            //welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor) -- ela so centraliza, se a view for maior que o tamalho do da tela ela nao eh exibida por completo
+//            
+//            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
+//            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -16),
+//            
+//            welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor,constant: 32),
+//            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 64),
+//            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -64),
+//            welcomeLabel.heightAnchor.constraint(equalToConstant: 64)
             
-            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,constant: 32),
-            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -64),
             
-            welcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor,constant: 32),
-            
-            //welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor) -- ela so centraliza, se a view for maior que o tamalho do da tela ela nao eh exibida por completo
-            
-            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
-            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -16),
-            
-            welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor,constant: 32),
-            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 64),
-            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -64),
-            welcomeLabel.heightAnchor.constraint(equalToConstant: 64)
+            welcomeButton.heightAnchor.constraint(equalToConstant: 64),
+            welcomeButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor,constant: 64),
+            welcomeButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor,constant: -64)
         ])
     }
 
--- Cinetopia/Cinetopia/SceneDelegate.swift
+++ Cinetopia/Cinetopia/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = HomeViewController()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController()) 
         
         
     }
